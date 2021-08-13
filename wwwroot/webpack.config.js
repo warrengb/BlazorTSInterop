fa2bc7957f51ecbce1a6ab85fa6b40ea014fc03a
gaslobster <w4rr3nGB>
2021-08-13 18:14:05 -0400

    add config

---
+++ wwwroot/webpack.config.js
@@ -0,0 +1,27 @@
+﻿const path = require("path");
+
+module.exports = {
+    mode: 'development',
+    devtool: 'eval-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(ts)$/,
+                exclude: /node_modules/,
+                include: [path.resolve(__dirname, 'src')],
+                use: 'ts-loader',
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    entry: {
+        index: ['./src/index']  
+    },
+    output: {
+        path: path.resolve(__dirname, '../wwwroot/public'),
+        filename: '[name]-bundle.js',
+
+    }
+};
